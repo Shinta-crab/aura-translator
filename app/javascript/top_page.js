72445ba3235112df9abe919c0962aa17840fecd5
Shinta-crab <scrab.engineer@gmail.com>
2025-10-13 04:21:56 +0900

    humberger menu 参考資料より

--- app/javascript/top_page.js
+++ app/javascript/top_page.js
@@ -1,13 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const menuToggle = document.getElementById("menu-toggle");
-  const menuClose = document.getElementById("menu-close");
-  const sideMenu = document.getElementById("side-menu");
+// document.addEventListener("DOMContentLoaded", () => {
+//   const menuToggle = document.getElementById("menu-toggle");
+//   const menuClose = document.getElementById("menu-close");
+//   const sideMenu = document.getElementById("side-menu");
 
-  menuToggle.addEventListener("click", () => {
-    sideMenu.classList.add("open");
-  });
+//   menuToggle.addEventListener("click", () => {
+//     sideMenu.classList.add("open");
+//   });
 
-  menuClose.addEventListener("click", () => {
-    sideMenu.classList.remove("open");
-  });
+//   menuClose.addEventListener("click", () => {
+//     sideMenu.classList.remove("open");
+//   });
+// });
+
+button.addEventListener('click', event => {
+  bars.classList.toggle('hidden')
+  xmark.classList.toggle('hidden')
+  menu.classList.toggle('translate-x-full')
 });
